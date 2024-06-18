f1bd86995ec8070ab0064583ca2655d4963ed6b2
AlexWilliamSilva <alexwilliamgomessilva168@gmail.com>
2024-06-18 13:51:22 -0300

    sql commit

--- database/script.sql
+++ database/script.sql
@@ -1,5 +1,3 @@
--- SQLBook: Markup
-
 -- SQLBook: Code
 -- CREATE DATABASE dbloja;
 USE dbloja;
@@ -7,18 +5,37 @@ CREATE TABLE cliente
 (
     id_cli INT(12) PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nome_cli varchar(50) not NULL,
-    rua VARCHAR(255) NOT NULL,
+    rua VARCHAR(50) NOT NULL,
     numero INT(12) NOT NULL,
-    bairro VARCHAR(100) NOT NULL,
     cep VARCHAR(10) NOT NULL,
-    cidade VARCHAR(100) NOT NULL,
-    uf VARCHAR(2) NOT NULL,
     telefone VARCHAR(15),
     documento VARCHAR(4)
 )
+
 CREATE TABLE pedido
 (
     id_ped int(12) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    id_cli int(12) FOREIGN KEY AUTO_INCREMENT NOT NULL,
-    data_
+    id_cli int(12) NOT NULL,
+    data_ped DATE,
+    prazo_entrega DATE,
+    FOREIGN KEY (id_cli) REFERENCES cliente(id_cli)
+);
+
+CREATE TABLE item_pedido
+(
+    id_ped int(12) NOT NULL,
+    id_prod int(12) NOT NULL,
+    quant int NOT NULL,
+    preco_unit decimal(10, 2) NOT NULL,
+    PRIMARY KEY (id_ped, id_prod),
+    FOREIGN KEY (id_ped) REFERENCES pedido(id_ped),
+    FOREIGN KEY (id_prod) REFERENCES produto(id_prod)
+);
+
+CREATE TABLE produto
+(
+    id_prod int(12) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nome_prod varchar(50) NOT NULL,
+    desc_prod varchar(150) NOT NULL,
+    preco_unit float(12) NOT NULL
 )
